824d5dd3a479d0ec36ed691664c12e018c153ba6
Bhaviksune <118323028+Bhaviksune@users.noreply.github.com>
2024-07-02 18:42:59 +0000

    done

---
+++ basic_js/01-variable.js
@@ -0,0 +1,7 @@
+const accountId=2
+let accountNumber="970560596"
+
+var name="riya"
+//var never used , only used let and const for the work 
+
+console.table([accountId,accountNumber,name]) //for printing various variables
